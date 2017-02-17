b8c83dfc9b2681e7e2efa72710bdf16af9d9dd1a
David Chapman <talk2dc@gmail.com>
2017-02-16 22:24:51 -0500

    Add smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,6 +58,16 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'daily-bible.herokuapp.com' }
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              ENV['SPARKPOST_SMTP_HOST'],
+    port:                 ENV['SPARKPOST_SMTP_PORT'],
+    domain:               ENV['SPARKPOST_SMTP_HOST'],
+    user_name:            ENV['SPARKPOST_SMTP_USERNAME'],
+    password:             ENV['SPARKPOST_SMTP_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
