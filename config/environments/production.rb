869dbca507f5afb51784f69b776e85c05abec248
David Chapman <talk2dc@gmail.com>
2017-02-16 23:13:17 -0500

    switch to sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,11 +60,10 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
+    address:              'smtp.sendgrid.net',
     port:                 587,
-    domain:               'gmail.com',
-    user_name:            ENV['GMAIL_USERNAME'],
-    password:             ENV['GMAIL_PASSWORD'],
+    user_name:            ENV['SENDGRID_USERNAME'],
+    password:             ENV['SENDGRID_PASSWORD'],
     authentication:       :login,
     enable_starttls_auto: true  }
 
